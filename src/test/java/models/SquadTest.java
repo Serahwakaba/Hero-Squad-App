c6eddf383fc9dd650a22b83f534057a4cc0bb174
Serah wakaba <serahwakaba9@gmail.com>
2021-06-06 16:18:10 +0300

    squadtest commit

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,21 +1,21 @@
-//package models;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-////import org.sql2o.Sql2o;
-//
-//import static org.junit.Assert.*;
-//
-//public class SquadTest {
-//    @Before
-//    public void setUp() throws Exception {
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        Squad.clearEntireSquad();
-//    }
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+//import org.sql2o.Sql2o;
+
+import static org.junit.Assert.*;
+
+public class SquadTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Squad.clearEntireSquad();
+    }
 //
 //    @Test
 //    public void squadIsCreatedCorrectly_true() throws Exception

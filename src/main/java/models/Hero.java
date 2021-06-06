b5ada1eea052086809e1326d7305faf665d0651f
Serah wakaba <serahwakaba9@gmail.com>
2021-06-06 15:02:05 +0300

    setters and getters commit

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -19,84 +19,84 @@ public class Hero {
         instances.add(this);
         this.id = instances.size();
     }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Hero hero = (Hero) o;
-//        return age == hero.age &&
-//                id == hero.id &&
-//                Objects.equals(name, hero.name) &&
-//                Objects.equals(specialPowers, hero.specialPowers) &&
-//                Objects.equals(weakness, hero.weakness);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(name, age, specialPowers, weakness, id);
-//    }
-//
-//    public static ArrayList<Hero> getAll() {
-//        return instances;
-//    }
-//
-//    public static void clearAllHeroes(){
-//        instances.clear();
-//    }
-//
-//    public static Hero findById(int id){
-//        return instances.get(id-1);
-//    }
-//
-//    public void update(String name, int age, String specialPowers, String weakness) {
-//        this.name = name;
-//        this.age=age;
-//        this.specialPowers=specialPowers;
-//        this.weakness=weakness;
-//    }
-//
-//    public void deleteHero(){
-//        instances.remove(id-1);
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//
-//    public String getSpecialPowers() {
-//        return specialPowers;
-//    }
-//
-//    public void setSpecialPowers(String specialPowers) {
-//        this.specialPowers = specialPowers;
-//    }
-//
-//    public String getWeakness() {
-//        return weakness;
-//    }
-//
-//    public void setWeakness(String weakness) {
-//        this.weakness = weakness;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return age == hero.age &&
+                id == hero.id &&
+                Objects.equals(name, hero.name) &&
+                Objects.equals(specialPowers, hero.specialPowers) &&
+                Objects.equals(weakness, hero.weakness);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, specialPowers, weakness, id);
+    }
+
+    public static ArrayList<Hero> getAll() {
+        return instances;
+    }
+
+    public static void clearAllHeroes(){
+        instances.clear();
+    }
+
+    public static Hero findById(int id){
+        return instances.get(id-1);
+    }
+
+    public void update(String name, int age, String specialPowers, String weakness) {
+        this.name = name;
+        this.age=age;
+        this.specialPowers=specialPowers;
+        this.weakness=weakness;
+    }
+
+    public void deleteHero(){
+        instances.remove(id-1);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecialPowers() {
+        return specialPowers;
+    }
+
+    public void setSpecialPowers(String specialPowers) {
+        this.specialPowers = specialPowers;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}

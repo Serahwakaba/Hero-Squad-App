21364edbdc164bc36e9f1130df6a986815416389
Serah wakaba <serahwakaba9@gmail.com>
2021-06-06 15:52:57 +0300

    heroIsCreatedCorrectly commit

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,28 +1,28 @@
-//package models;
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.http.HttpRequest;
+
+import static org.junit.Assert.*;
+
+public class HeroTest {
+    @Before
+    public void setUp() throws Exception {
+    }
 //
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.net.http.HttpRequest;
-//
-//import static org.junit.Assert.*;
-//
-//public class HeroTest {
-//    @Before
-//    public void setUp() throws Exception {
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        Hero.clearAllHeroes();
-//    }
-//    @Test
-//    public void heroIsCreatedCorrectly_true() throws Exception
-//    {
-//        Hero hero=createNewHero();
-//        assertEquals(true,hero instanceof Hero);
-//    }
+    @After
+    public void tearDown() throws Exception {
+        Hero.clearAllHeroes();
+    }
+    @Test
+    public void heroIsCreatedCorrectly_true() throws Exception
+    {
+        Hero hero=createNewHero();
+        assertEquals(true,hero instanceof Hero);
+    }
 //    @Test
 //    public void heroInstantiatesCorrectly() throws Exception {
 //        Hero hero=createNewHero();
